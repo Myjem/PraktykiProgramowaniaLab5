347e04869afb30a150adb10839e6f7b1faca9693
Myjem <siepkawiktor240@gmail.com>
2025-05-07 16:48:01 +0200

    added requirements

--- utils.py
+++ utils.py
@@ -1,17 +1,21 @@
 # Exemplary calculator functions
-
+'''test'''
 
 def add(a: int, b: int) -> int:
+    '''test'''
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
+    '''test'''
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
+    '''test'''
     return a * b
 
 
 def divide(a: int, b: int) -> float:
+    '''test'''
     return a / b
